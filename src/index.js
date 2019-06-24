0382d1c90a8168896baf07a07efd5f0028769bf3
Aishwarya <aishwarya.p@razorthink.com>
2019-06-24 11:37:58 +0530

    react basic modules added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import "./style.scss";
 class App extends React.Component {
     render() {
         return <h1>Hello World from React boilerplate</h1>;
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
+    devtool: 'inline-source-map',
     entry: './src/index.js',
     output: {
-        path: __dirname + '/dist',
+        path: path.resolve(__dirname, 'build'),
         publicPath: '/',
         filename: 'bundle.js'
     },
     devServer: {
-        contentBase: './dist',
+        contentBase: "./build",
     },
     module: {
         rules: [
@@ -14,7 +17,20 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: ['babel-loader']
-            }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
         ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve('./index.html'),
+        }),
+    ]
 };
