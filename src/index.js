63f7d5dc64e7409e99764ad12a692f586422b377
Aishwarya <aishwarya.p@razorthink.com>
2019-06-25 10:53:13 +0530

    new changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import styles from "./style.scss";
 class App extends React.Component {
     render() {
-        return <h1 className={styles.heading}>Hello Kitty !!! </h1>;
+        return <h1>Hello Kitty !!! </h1>;
     }
 }
 ReactDOM.render(<App />, document.getElementById("root"));
